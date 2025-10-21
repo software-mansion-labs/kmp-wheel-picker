d8567c92b6f1c3df8ad4f8c50e7a0f9d8215072c
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2025-10-21 20:46:54 +0200

    Sample app: Enable edge-to-edge on Android

--- sample/src/commonMain/kotlin/com/patrykandpatrick/kovo/sample/App.kt
+++ sample/src/commonMain/kotlin/com/patrykandpatrick/kovo/sample/App.kt
@@ -1,11 +1,15 @@
 package com.patrykandpatrick.kovo.sample
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,14 +22,14 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            WheelPicker(
-                highlight = {
-                    Box(modifier = Modifier.border(2.dp, Color.DarkGray, RoundedCornerShape(8.dp)))
+    MaterialTheme(if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()) {
+        Scaffold { padding ->
+            Box(Modifier.fillMaxWidth().padding(padding), Alignment.TopCenter) {
+                WheelPicker(
+                    highlight = { Box(Modifier.border(2.dp, Color.Gray, RoundedCornerShape(8.dp))) }
+                ) {
+                    repeat(10) { index -> WheelPickerItem("Item $index") }
                 }
-            ) {
-                repeat(10) { WheelPickerItem("Item $it") }
             }
         }
     }
