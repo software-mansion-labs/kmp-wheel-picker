3d4483738f398c398da6f3281e33d5e0bff1608e
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2025-10-21 20:39:15 +0200

    Sample app: Replace Material 2 with Material 3

--- sample/src/commonMain/kotlin/com/patrykandpatrick/kovo/sample/App.kt
+++ sample/src/commonMain/kotlin/com/patrykandpatrick/kovo/sample/App.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
