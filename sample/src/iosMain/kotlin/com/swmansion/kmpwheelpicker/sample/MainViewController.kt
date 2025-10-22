9acf8e98502755271fc1717060e29ff71e880ab2
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2025-10-22 12:52:46 +0200

    Rename `App` to `SampleApp`

--- sample/src/iosMain/kotlin/com/swmansion/kmpwheelpicker/sample/MainViewController.kt
+++ sample/src/iosMain/kotlin/com/swmansion/kmpwheelpicker/sample/MainViewController.kt
@@ -2,4 +2,4 @@ package com.swmansion.kmpwheelpicker.sample
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { SampleApp() }
